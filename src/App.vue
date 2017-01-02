d2381cdf6ebc0ac1dfee59606eaa938c179aea56
jewelz <hujewelz@163.com>
2017-01-02 20:52:40 +0800

    initial project

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <button @click="addNew" type="submit" class="btn btn-success">add new</button>
+    <hello :datas="datas" @submit='addNew'></hello>
+    <div class="mask" :class="{addnew: isAddNew}">
+      <div class="container">
+        <form class="form-horizontal" role="form" @submit.prevent="submit">
+          <div class="form-group">
+            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+            <div class="col-md-10">
+              <input v-model="newItem" type="text" class="form-control"  id="inputEmail3" placeholder="Username">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-md-offset-2">
+              <button type="submit" class="btn btn-default">Sign in</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'To do list',
+      isAddNew: false,
+      datas: [],
+      newItem: ''
+    }
+  },
+  methods: {
+    addNew () {
+      this.isAddNew = true
+      console.log('add new item')
+    },
+    submit () {
+      this.isAddNew = false
+      this.datas.push(this.newItem)
+      this.newItem = ''
+    }
+  },
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+
+@import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.mask {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  top: 0;
+  width: 100%;
+  height: 100%;
+  visibility: hidden;
+}
+
+.addnew {
+  visibility: visible;;
+}
+
+.center {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: green;
+}
+
+form {
+  margin-top: 400px;
+}
+
+.form-horizontal .form-group {
+     margin-left: 0;
+    /* margin-left: -15px;*/
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <div class="col-md-12">
+        <ul class="list-group">
+          <li class="list-group-item" v-for="data in datas">
+            <a href="#" target="_blank">{{data}}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props: ['datas'],
+  methods: {
+    submit () {
+      this.$emit('submit')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+h1, h2 {
+  font-weight: normal;
+}
+
+
+.list-group-item {
+  text-align: left;
+}
+
+
+/*a {
+  color: #42b983;
+}*/
+</style>
